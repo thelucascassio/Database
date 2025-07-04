362fb046ebcb183a5a13e54bab2f91daeae522b8
Lucas Cássio Araújo Oliveira <20231011110002@ifrn.local>
2025-07-04 15:47:00 -0300

    Adicionei os arquivos de exercícios solicitados por Papaiz

---
+++ AulasBD_v2/app/views.py
@@ -0,0 +1,164 @@
+from django.shortcuts import render
+
+import pyodbc
+
+
+def obter_conexao():
+    # define os parametros de conexao
+    driver   = '{ODBC Driver 17 for SQL Server}'
+    servidor = '.\SQLEXPRESS'
+    banco    = 'Aulas_BD'
+    usuario  = 'sa'
+    senha    = 'Senha@123' # poder ser também: "senha", "senha@123", "Senha@123"
+
+    # realiza conexao com o BD
+    string_conexao = f'Driver={driver};Server={servidor};Database={banco};UID={usuario};PWD={senha}'
+    conexao = pyodbc.connect(string_conexao)
+    
+    # retorna a conexao  
+    return conexao
+
+
+def home(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'home.html'
+    return render(request, template)
+
+def exercicio_1(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'exercicio_1.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # define o comando SQL que será executado
+        sql = '''
+            SELECT  dep.nome as 'departamento',
+                    fun.nome, 
+                    fun.telefones
+
+            FROM Funcionario fun
+            INNER JOIN Departamento dep ON dep.id = fun.departamento_id
+
+            ORDER BY dep.nome, fun.nome
+        '''
+        
+        # usa o cursor para executar o SQL
+        cursor.execute(sql)
+        # obtem todos os registros retornados
+        registros = cursor.fetchall()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, context={'registros': registros})
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
+
+def exercicio_2(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'exercicio_2.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # define o comando SQL que será executado
+        sql = '''
+            SELECT 
+            Turma.nome AS nome_turma,
+            Aluno.nome AS nome_aluno,
+            Aluno.idade
+            FROM 
+                Aluno
+            JOIN 
+                Turma ON Aluno.turma_id = Turma.id
+            ORDER BY 
+                Turma.nome,
+                Aluno.nome;
+        '''
+        
+        # usa o cursor para executar o SQL
+        cursor.execute(sql)
+        # obtem todos os registros retornados
+        registros = cursor.fetchall()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, context={'registros': registros})
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
+
+def exercicio_3(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'exercicio_1.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # define o comando SQL que será executado
+        sql = '''
+            SELECT  dep.nome as 'departamento',
+                    fun.nome, 
+                    fun.telefones
+
+            FROM Funcionario fun
+            INNER JOIN Departamento dep ON dep.id = fun.departamento_id
+
+            ORDER BY dep.nome, fun.nome
+        '''
+        
+        # usa o cursor para executar o SQL
+        cursor.execute(sql)
+        # obtem todos os registros retornados
+        registros = cursor.fetchall()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, context={'registros': registros})
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
+
+def exercicio_4(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'exercicio_1.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # define o comando SQL que será executado
+        sql = '''
+            SELECT  dep.nome as 'departamento',
+                    fun.nome, 
+                    fun.telefones
+
+            FROM Funcionario fun
+            INNER JOIN Departamento dep ON dep.id = fun.departamento_id
+
+            ORDER BY dep.nome, fun.nome
+        '''
+        
+        # usa o cursor para executar o SQL
+        cursor.execute(sql)
+        # obtem todos os registros retornados
+        registros = cursor.fetchall()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, context={'registros': registros})
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
